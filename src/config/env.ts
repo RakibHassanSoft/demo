26f750a5c62bf9e5e3b9a139004f20f65f42502a
RakibHassanSoft <rakibreings32@gmail.com>
2026-06-10 17:43:00 +0600

    Demo

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,4 +7,5 @@ export const env = {
   DATABASE_URL: process.env.DATABASE_URL!,
   JWT_SECRET: process.env.JWT_SECRET!,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!,
+  NODE_ENV : process.env.NODE_ENV || "development",
 };
